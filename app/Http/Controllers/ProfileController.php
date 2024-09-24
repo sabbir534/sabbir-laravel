1cc98df4daa2a7828bafd9ffa3c8e963f7f45a5f
Sabbir Ahmed <sabbir.apfl@akijassets.com>
2024-09-24 13:55:56 +0600

    first commit

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ProfileController extends Controller
+{
+    public function index($id)
+    {
+        $name = "Donal Trump";
+        $age = "75";
+
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age,
+        ];
+
+        $cookie_name = 'access_token';
+        $value = '123-XYZ';
+        $minutes = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+
+        return response()->json($data, 200)
+            ->cookie($cookie_name, $value, $minutes, $path, $domain, $secure, $httpOnly);
+    }
+}
